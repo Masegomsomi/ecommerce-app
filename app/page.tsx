1de8c034ff60b0d3c29496f055ee08322033301c
Masegolegendsystems <masego@legendsystems.co.za>
2023-08-30 00:29:02 +0200

    Sorting items

--- app/page.tsx
+++ app/page.tsx
@@ -9,10 +9,19 @@ import { ProductGrid } from "@/components/product-grid"
 import { ProductSort } from "@/components/product-sort"
 import { seedSanityData } from "@/lib/seed"
 
-interface Props {}
+interface Props {
+  searchParams:{
+    date?: string
+    price?: string
+  }
+}
 
-export default async function Page() {
- const products = await client.fetch<SanityProduct[]>(groq`*[_type == "product"] {
+export default async function Page({searchParams}: Props) {
+  const priceOrder = searchParams.price ?`| order(price ${searchParams.price})` : ""
+  const dateOrder = searchParams.date ?`| order(_createdAt ${searchParams.price})` : ""
+ const order = `${priceOrder}${dateOrder}`
+ const products = await client.fetch<SanityProduct[]>(
+  groq`*[_type == "product"] ${order} {
  _id,
  _createdAt, 
  name,
