13abc1ffd1700c15c4ed24495700d826fb9094ad
Masegolegendsystems <masego@legendsystems.co.za>
2023-08-29 21:16:08 +0200

    first commit

---
+++ config/site.ts
@@ -0,0 +1,14 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Messeri",
+  description: "Trendy styles suitable for everyday life",
+  footer: [
+    { name: "Home", href: "/" },
+    { name: "About", href: "#" },
+    { name: "Terms & Conditions", href: "#" },
+    { name: "Shipping & Return Policy", href: "#" },
+    { name: "Privacy Policy", href: "#" },
+    { name: "FAQ", href: "#" },
+  ],
+}
