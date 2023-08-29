0e9a66345d9dcecd8218fc636c3431dfd5ac1965
Masegolegendsystems <masego@legendsystems.co.za>
2023-08-29 22:20:24 +0200

    setting up sanity

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,4 +7,6 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
+  token:
+  "skzsXvzwZjrj1jdksTAhrHFTL6lDDsWKBENPpBeFBQkZTvkhd37d0b4b3fXZO1uXOAV8vkX8OW6ekGEzUds4G08vwez79pbqcSr9GabRnvIc8vmWktMrCtdTfDjA6aXEBFnEppAetUrOuNmEWG5Y5eTPBuNxAGh6eT9s4sRfa6dtpUJmHuVR",
 })
