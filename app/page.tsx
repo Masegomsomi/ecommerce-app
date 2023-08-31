12e71bef245e88426898ca231c3b1e29110f5de5
Masegolegendsystems <masego@legendsystems.co.za>
2023-08-31 03:06:11 +0200

    filtering complete

--- app/page.tsx
+++ app/page.tsx
@@ -18,17 +18,17 @@ interface Props {
     size?: string
   }
 }
-
-export default async function Page({searchParams}: Props) {
+                       
+export default async function Page({searchParams}: Props){
   const {date = "desc", price, color, category, size} = searchParams
   const priceOrder = price ?`| order(price ${price})` : ""
   const dateOrder = date ?`| order(_createdAt ${date})` : ""
  const order = `${priceOrder}${dateOrder}`
 
- const productFilter = `_type == "product" &&`
- const colorFilter = color ? `&& "${color}" in colors`: ""
- const categoryFilter = category ? `&& "${category}" in categories`: ""
- const sizeFilter = size ? `&& "${size}" in sizes`: ""
+ const productFilter = `_type == "product"`
+ const colorFilter = color ? `&& "${color}" in colors` : ""
+ const categoryFilter = category ? `&& "${category}" in categories` : ''
+ const sizeFilter = size ? `&& "${size}" in sizes` : ''
  const filter = `*[${productFilter}${colorFilter}${categoryFilter}${sizeFilter}]`
 
 
@@ -44,7 +44,6 @@ export default async function Page({searchParams}: Props) {
  price,
  description, 
  "slug": slug.current
-
  }`
  )
 
